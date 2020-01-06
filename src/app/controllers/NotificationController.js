9c7a68a689e8f2a694e180fceee97d0f1a98b55d
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2020-01-06 16:43:19 -0300

    marcando notificaçoes como lidas

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -19,4 +19,15 @@ class NotificationController {
 
         return res.json(notifications);
     }
+
+    async update(req, res){
+        const notifications = await Notification.findByIdAndUpdate(
+            req.params.id,
+            { read: true },
+            { new: true }
+        );
+
+        return res.json(notifications);
+    }
+    
 } export default new NotificationController();
