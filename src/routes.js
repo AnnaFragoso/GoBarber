38da2011e7370f64e43d5f94ca929fa310d6ae48
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2020-01-06 13:11:19 -0300

    listagem da agenda do prestador de serviços

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,8 +23,10 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/appointments', AppointmentController.store);
+routes.post('/appointments', AppointmentController.store);;
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedules', ScheduleController.index)
 
 
 export default routes;
